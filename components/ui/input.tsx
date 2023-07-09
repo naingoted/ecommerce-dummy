3b82940348ae37b207fcc0bf71279eb4b6202981
vincent.thet <naingoted@gmail.com>
2023-07-09 21:07:39 +0800

    [responsive] refactoring [unit-test] refactoring

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -10,16 +10,17 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     const inputId = `${label.replace(" ", "")}-${type}`;
 
     return (
-      <div>
+      <div className="max-w-[360px] mx-auto w-full">
         <label
           htmlFor={inputId}
-          className="block text-sm font-medium text-gray-700"
+          className="block text-md font-bold text-gray-700"
         >
           {label}
         </label>
         <input
           type={type}
           id={inputId}
+          role="input"
           aria-label={label}
           className={cn(
             "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm",
